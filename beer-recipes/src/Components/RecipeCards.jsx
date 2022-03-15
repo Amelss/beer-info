c23d48ad29a36b3a549106a4973b590b1ed74d3c
Amelss <ameleyka@gmail.com>
2022-03-15 14:49:18 +0000

    simplified app, just had two components

--- beer-recipes/src/Components/RecipeCards.jsx
+++ beer-recipes/src/Components/RecipeCards.jsx
@@ -25,57 +25,76 @@ export default function RecipeCards() {
        
     }, []);
     
+  const handleClick = (e) => {
+    e.preventDefault();
+    getBeers();
+  };
   
   return (
     <div className="px-5 py-10">
       <div className="bg-slate-100 rounded shadow-md pb-5 md:mx-96 ">
         {beerData.map((beers) => (
           <div key={id}>
-            <div className='block md:flex'>
-            <h1 className="text-center text-1xl py-4 md:hidden">{beers.name}</h1>
-            <img
-              src={beers.image_url}
-              alt="beer"
-              className="h-44 mx-auto md:h-80 md:pt-7 px-10"
+            <div className="block md:flex">
+              <h1 className="text-center text-1xl py-4 md:hidden">
+                {beers.name}
+              </h1>
+              <img
+                src={beers.image_url}
+                alt="beer"
+                className="h-44 mx-auto md:h-80 md:pt-7 px-10"
               />
               <div>
-            <h1 className="hidden md:block text-3xl py-4 px-3">{beers.name}</h1>
-            <p className="pt-5 text-xs px-3 md:text-sm">Tagline: {beers.tagline}</p>
-            <p className="pt-3 text-xs text-gray-500 px-3 ">
-              First Brew Date: {beers.first_brewed}
-            </p>
-            <p className="mt-1 text-sm px-3 font-light md:text-sm">{beers.description}</p>
-            <p className="mt-1 text-sm px-3 font-light md:text-sm">
-              <span className="font-medium">ABV:</span> {beers.abv}
-            </p>
-            <p className="mt-1 px-3">Ingredients:</p>
-            <p className="mt-1 text-sm px-3 font-light">
-              {beers.ingredients.malt[0].name}
-            </p>
-            <p className="mt-1 text-sm px-3 font-light">
-              {beers.ingredients.malt[0].amount.value}
-            </p>
-            <p className="mt-1 text-sm px-3 font-light">
-              {beers.ingredients.malt[0].amount.unit}
-            </p>
-            <p className="mt-1 px-3">Food Pairings:</p>
-            <p className="mt-1 text-sm px-3 font-light">
-              {" "}
-              - {beers.food_pairing[0]}
-            </p>
-            <p className="mt-1 text-sm px-3 font-light">
-              {" "}
-              - {beers.food_pairing[1]}
-            </p>
-            <p className="mt-1 text-sm px-3 font-light">
-              {" "}
-              - {beers.food_pairing[2]}
+                <h1 className="hidden md:block text-3xl py-4 px-3">
+                  {beers.name}
+                </h1>
+                <p className="pt-5 text-xs px-3 md:text-sm">
+                  Tagline: {beers.tagline}
+                </p>
+                <p className="pt-3 text-xs text-gray-500 px-3 ">
+                  First Brew Date: {beers.first_brewed}
+                </p>
+                <p className="mt-1 text-sm px-3 font-light md:text-sm">
+                  {beers.description}
+                </p>
+                <p className="mt-1 text-sm px-3 font-light md:text-sm">
+                  <span className="font-medium">ABV:</span> {beers.abv}
+                </p>
+                <p className="mt-1 px-3">Ingredients:</p>
+                <p className="mt-1 text-sm px-3 font-light">
+                  {beers.ingredients.malt[0].name}
+                </p>
+                <p className="mt-1 text-sm px-3 font-light">
+                  {beers.ingredients.malt[0].amount.value}
+                </p>
+                <p className="mt-1 text-sm px-3 font-light">
+                  {beers.ingredients.malt[0].amount.unit}
+                </p>
+                <p className="mt-1 px-3">Food Pairings:</p>
+                <p className="mt-1 text-sm px-3 font-light">
+                  {" "}
+                  - {beers.food_pairing[0]}
+                </p>
+                <p className="mt-1 text-sm px-3 font-light">
+                  {" "}
+                  - {beers.food_pairing[1]}
+                </p>
+                <p className="mt-1 text-sm px-3 font-light">
+                  {" "}
+                  - {beers.food_pairing[2]}
                 </p>
               </div>
             </div>
           </div>
         ))}
       </div>
+
+      <button
+        onClick={handleClick}
+        className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800 font-light rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Click For Another Beer{" "}
+      </button>
     </div>
   );
 }
